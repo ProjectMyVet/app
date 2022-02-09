aefcbb9d826d54c96799ece7e893b23b93fafa08
Rhian Lopes da Costa <rhian.costa@MacBook-Pro-de-Rhian.local>
2022-02-09 20:58:43 -0300

    feat: introducing attendence screen

--- src/screens/AttendenceDetail/index.js
+++ src/screens/AttendenceDetail/index.js
@@ -1,14 +1,37 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, ScrollView } from 'react-native'
 import { MVText } from '../../components'
 
 import styles from './styles'
 
-export function AttendenceDetailScreen({ navigation }) {
+export function AttendenceDetailScreen({ navigation, route }) {
+  const [attendence, setAttendence] = useState({})
 
-    return (
-        <View style={styles.container}>
-          <MVText style={styles.title}>Detalhe Atendimento</MVText>
+  useEffect(() => {
+    setAttendence(route.params.attendence)
+    console.log(route.params.attendence)
+  },[])
+
+  return (
+    <>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.field}>
+          <MVText style={styles.label}>Nome Tutor:</MVText>
+          <MVText style={styles.value}>{attendence.customerName}</MVText>
         </View>
-    );
+        <View style={styles.field}>
+          <MVText style={styles.label}>Nome Veterinário:</MVText>
+          <MVText style={styles.value}>{attendence.vetName}</MVText>
+        </View>
+        <View style={styles.field}>
+          <MVText style={styles.label}>Pet:</MVText>
+          <MVText style={styles.value}>{attendence.pet['name']}</MVText>
+        </View>
+        <View style={styles.field}>
+          <MVText style={styles.label}>Idade Pet:</MVText>
+          <MVText style={styles.value}>{attendence.vetName}</MVText>
+        </View>
+      </ScrollView>
+    </>
+  )
 }
--- src/screens/AttendenceDetail/styles.js
+++ src/screens/AttendenceDetail/styles.js
@@ -1,16 +1,30 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Dimensions } from 'react-native'
 import { COLORS } from '../../constants';
+
+const { width, height } = Dimensions.get('window')
 
 export default StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        paddingTop: height / 15,
+        flex: 1,
+        paddingRight: 15,
+        paddingLeft: 15,
     },
     title: {
         fontSize: 22,
         color: COLORS.BLACK,
     },
+    label: {
+        fontSize: 16,
+        color: COLORS.BLACK,
+    },
+    value: {
+        fontSize: 20,
+        color: COLORS.BLACK,
+    },
+    field: {
+        marginTop: 15
+    }
 });
   
