eea2e5a671fb0f55b11926ae6dd73d8bb98154be
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2022-06-15 20:44:44 -0300

    Update index.js

--- environment/index.js
+++ environment/index.js
@@ -1,4 +1,4 @@
 export const ENV = {
-    IOS_CLIENT_ID: '157470305510-g9s47agdpsdg8tn2v610bq8t9nkdm939.apps.googleusercontent.com',
-    ANDROID_CLIENT_ID: '157470305510-caaei5885p5oo34mf1ohjucr0n9r942s.apps.googleusercontent.com'
+    IOS_CLIENT_ID: '<insert>',
+    ANDROID_CLIENT_ID: '<insert>'
 }
